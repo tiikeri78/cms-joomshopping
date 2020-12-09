76ab7f705bf21229f8ee296c6914c09ffe3e1117
Pavel Molodovsky <pamolodovskii@yoomoney.ru>
2020-12-09 12:03:34 +0300

    CMSSDK-1542: ребрендинг модуля

---
+++ src/components/com_jshopping/payments/pm_yoomoney/lib/autoload.php
@@ -0,0 +1,30 @@
+<?php
+
+if (!defined('DS')) {
+    define('DS', DIRECTORY_SEPARATOR);
+}
+
+require_once dirname(__FILE__) . DS . 'yookassa-sdk-php/lib' . DS . 'autoload.php';
+
+define('YOOMONEY_MODULE_PATH', dirname(__FILE__));
+
+function yooMoneyLoadClass($className)
+{
+    if (strncmp('YooMoneyModule', $className, 14) === 0) {
+        $path = YOOMONEY_MODULE_PATH;
+        $length = 11;
+    } else {
+        return;
+    }
+    if (DIRECTORY_SEPARATOR === '/') {
+        $path .= str_replace('\\', '/', substr($className, $length));
+    } else {
+        $path .= substr($className, $length);
+    }
+    $path .= '.php';
+    if (file_exists($path)) {
+        include $path;
+    }
+}
+
+spl_autoload_register('yooMoneyLoadClass');
